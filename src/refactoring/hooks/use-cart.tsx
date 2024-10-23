58664523c723a93c2843e02d46943e19983f1d8f
codeeon <yeon.develop@gmail.com>
2024-10-23 23:54:09 +0900

    Refactor: cart-utils, use-cart 함수 분리

--- src/refactoring/hooks/use-cart.tsx
+++ src/refactoring/hooks/use-cart.tsx
@@ -1,39 +1,46 @@
 // useCart.ts
-import { Dispatch, SetStateAction, useState } from 'react';
+import { useState } from 'react';
 import type { CartItemType, CouponType, ProductType } from '../../types';
-import { calculateCartTotal, updateCartItemQuantity } from './utils/cart-utils';
+import {
+  calculateCartTotal,
+  removeCartItem,
+  selectedCartItem,
+  updateCartItemQuantity,
+} from './utils/cart-utils';
 
 export const useCart = () => {
   const [cart, setCart] = useState<CartItemType[]>([]);
   const [selectedCoupon, setSelectedCoupon] = useState<CouponType | null>(null);
 
+  // Cart에 의존하는 계산 로직 - useCart에서의 const로 선언된 함수들.
   const getRemainingStock = (product: ProductType) => {
-    const cartItem = cart.find((item) => item.product.id === product.id);
+    const cartItem = selectedCartItem(cart, product.id);
     return product.stock - (cartItem?.quantity || 0);
   };
 
-  const updateCartItemQuantity = (productId: string, newQuantity: number) => {
-    return cart.map((item) => {
-      if (item.product.id === productId) {
-        return { ...item, quantity: newQuantity };
-      }
-      return item;
-    });
-  };
-
   const addNewCartItem = (product: ProductType) => {
-    const existingItem = cart.find((item) => item.product.id === product.id);
+    const existingItem = selectedCartItem(cart, product.id);
     if (existingItem) {
-      return updateCartItemQuantity(product.id, existingItem.quantity + 1);
+      return updateCartItemQuantity(cart, product.id, existingItem.quantity + 1);
     } else {
       return [...cart, { product, quantity: 1 }];
     }
   };
 
-  const removeCartItem = (productId: string) => {
-    return cart.filter((item) => item.product.id !== productId);
+  const calculateTotal = () => {
+    const { totalBeforeDiscount, totalAfterDiscount, totalDiscount } = calculateCartTotal(
+      cart,
+      selectedCoupon
+    );
+
+    return {
+      totalBeforeDiscount,
+      totalAfterDiscount,
+      totalDiscount,
+    };
   };
 
+  // 액션 함수(렌더링 조건에 따라 상태를 변경하는 함수들) - useCart에서의 function으로 선언된 함수들.
   function addToCart(product: ProductType) {
     const remainingStock = getRemainingStock(product);
     if (remainingStock <= 0) return;
@@ -44,63 +51,18 @@ export const useCart = () => {
   }
 
   function removeFromCart(productId: string) {
-    const cartWithoutItem = removeCartItem(productId);
+    const cartWithoutItem = removeCartItem(cart, productId);
 
     setCart(cartWithoutItem);
   }
 
   function updateQuantity(productId: string, newQuantity: number) {
-    setCart((prevCart) =>
-      prevCart
-        .map((item) => {
-          if (item.product.id === productId) {
-            const maxQuantity = item.product.stock;
-            const updatedQuantity = Math.max(0, Math.min(newQuantity, maxQuantity));
-            return updatedQuantity > 0 ? { ...item, quantity: updatedQuantity } : null;
-          }
-          return item;
-        })
-        .filter((item) => item !== null)
-    );
+    setCart((prevCart) => updateCartItemQuantity(prevCart, productId, newQuantity));
   }
 
-  const applyCoupon = (coupon: CouponType) => {
+  function applyCoupon(coupon: CouponType) {
     setSelectedCoupon(coupon);
-  };
-
-  const calculateTotal = () => {
-    let totalBeforeDiscount = 0;
-    let totalAfterDiscount = 0;
-
-    cart.forEach((item) => {
-      const { price } = item.product;
-      const { quantity } = item;
-      totalBeforeDiscount += price * quantity;
-
-      const discount = item.product.discounts.reduce((maxDiscount, d) => {
-        return quantity >= d.quantity && d.rate > maxDiscount ? d.rate : maxDiscount;
-      }, 0);
-
-      totalAfterDiscount += price * quantity * (1 - discount);
-    });
-
-    let totalDiscount = totalBeforeDiscount - totalAfterDiscount;
-
-    if (selectedCoupon) {
-      if (selectedCoupon.discountType === 'amount') {
-        totalAfterDiscount = Math.max(0, totalAfterDiscount - selectedCoupon.discountValue);
-      } else {
-        totalAfterDiscount *= 1 - selectedCoupon.discountValue / 100;
-      }
-      totalDiscount = totalBeforeDiscount - totalAfterDiscount;
-    }
-
-    return {
-      totalBeforeDiscount: Math.round(totalBeforeDiscount),
-      totalAfterDiscount: Math.round(totalAfterDiscount),
-      totalDiscount: Math.round(totalDiscount),
-    };
-  };
+  }
 
   return {
     cart,
--- src/refactoring/hooks/utils/cart-utils.ts
+++ src/refactoring/hooks/utils/cart-utils.ts
@@ -1,5 +1,27 @@
 import type { CartItemType, CouponType } from '../../../types';
 
+// params만 맞으면, 범용적으로 사용 가능한 계산식
+// export 함수들은 모두 외부에서도 사용 중.
+// export가 없는 함수들은, cart-utils 내부에서만 사용되는 함수들.
+export const selectedCartItem = (cart: CartItemType[], productId: string) => {
+  return cart.find((item) => item.product.id === productId);
+};
+
+export const getMaxApplicableDiscount = (item: CartItemType) => {
+  const { discountList } = item.product;
+  const { quantity } = item;
+
+  let appliedDiscount = 0;
+
+  Object.entries(discountList).forEach(([_, value]) => {
+    if (quantity >= value.quantity) {
+      appliedDiscount = Math.max(appliedDiscount, value.rate);
+    }
+  });
+
+  return appliedDiscount;
+};
+
 export const calculateItemTotal = (item: CartItemType) => {
   const { product, quantity } = item;
 
@@ -9,40 +31,32 @@ export const calculateItemTotal = (item: CartItemType) => {
   return total * (1 - discount);
 };
 
-export const getMaxApplicableDiscount = (item: CartItemType) => {
-  const { discounts } = item.product;
-  const { quantity } = item;
-
-  let appliedDiscount = 0;
-
-  Object.entries(discounts).forEach(([_, value]) => {
-    if (quantity >= value.quantity) {
-      appliedDiscount = Math.max(appliedDiscount, value.rate);
-    }
-  });
-
-  return appliedDiscount;
-};
-
-export const calculateTotalBeforeDiscount = (cart: CartItemType[]) =>
+const calculateTotalBeforeDiscount = (cart: CartItemType[]) =>
   cart.reduce((acc, item) => acc + item.product.price * item.quantity, 0);
 
-export const calculateCartTotal = (cart: CartItemType[], selectedCoupon: CouponType | null) => {
-  const totalBeforeDiscount = calculateTotalBeforeDiscount(cart);
-
-  let totalAfterDiscount = cart.reduce((acc, item) => acc + calculateItemTotal(item), 0);
+const calculateCartTotalAfterDiscount = (
+  cart: CartItemType[],
+  selectedCoupon?: CouponType | null
+) => {
+  let totalItemAmount = cart.reduce((acc, item) => acc + calculateItemTotal(item), 0);
 
   if (selectedCoupon) {
     switch (selectedCoupon?.discountType) {
       case 'amount':
-        totalAfterDiscount -= selectedCoupon.discountValue;
+        totalItemAmount -= selectedCoupon.discountValue;
         break;
       case 'percentage':
-        totalAfterDiscount *= 1 - selectedCoupon.discountValue / 100;
+        totalItemAmount *= 1 - selectedCoupon.discountValue / 100;
         break;
     }
   }
 
+  return totalItemAmount;
+};
+
+export const calculateCartTotal = (cart: CartItemType[], selectedCoupon: CouponType | null) => {
+  const totalBeforeDiscount = calculateTotalBeforeDiscount(cart);
+  const totalAfterDiscount = calculateCartTotalAfterDiscount(cart, selectedCoupon);
   const totalDiscount = totalBeforeDiscount - totalAfterDiscount;
 
   return {
@@ -52,22 +66,23 @@ export const calculateCartTotal = (cart: CartItemType[], selectedCoupon: CouponT
   };
 };
 
+export const removeCartItem = (cart: CartItemType[], productId: string) => {
+  return cart.filter((item) => item.product.id !== productId);
+};
+
 export const updateCartItemQuantity = (
   cart: CartItemType[],
   productId: string,
   newQuantity: number
 ): CartItemType[] => {
   if (newQuantity <= 0) {
-    return cart.filter((item) => item.product.id !== productId);
+    return removeCartItem(cart, productId);
   }
 
   return [
     {
-      product: cart.find((item) => item.product.id === productId)!.product,
-      quantity: Math.min(
-        newQuantity,
-        cart.find((item) => item.product.id === productId)!.product.stock
-      ),
+      product: selectedCartItem(cart, productId)!.product,
+      quantity: Math.min(newQuantity, selectedCartItem(cart, productId)!.product.stock),
     },
     ...cart.filter((item) => item.product.id !== productId),
   ];
